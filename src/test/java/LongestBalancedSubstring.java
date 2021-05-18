4678929462d8ad96ee77b2a9de89becc5ae0b538
Karan Chaudhary <karan.c.aux@go-jek.com>
2021-05-18 11:31:29 +0530

    Karan | Added max product triplet and longest balanced substring

---
+++ src/test/java/LongestBalancedSubstring.java
@@ -0,0 +1,36 @@
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestBalancedSubstring {
+
+    String input = "0100110";
+
+    @Test
+    public void testLongestBalancedSubstring() {
+        System.out.println(getLongestSubstring(input));
+    }
+
+    String getLongestSubstring(String s) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int count = 0, leftIndex = 0, rightIndex = 0, maxLen = 0;
+
+        char[] charArray = s.toCharArray();
+        for (int i = 0, charArrayLength = charArray.length; i < charArrayLength; i++) {
+            int each = Integer.parseInt(String.valueOf(charArray[i]));
+            count += (each == 1) ? 1 : -1;
+            if (map.containsKey(count)) {
+                leftIndex = map.get(count);
+                maxLen = Math.max(maxLen, i - leftIndex);
+                rightIndex = i;
+            } else
+                map.put(count, i);
+        }
+
+        return s.substring(leftIndex, rightIndex);
+
+    }
+}
---
+++ src/test/java/MaxProductTriplet.java
@@ -0,0 +1,22 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class MaxProductTriplet {
+
+    @Test
+    public void testTripletMaxProduct() {
+        int[] arr = {9, -2, -3, -2, 4, 5, 0};
+        System.out.println(getMaxProductTriplet(arr));
+    }
+
+    int getMaxProductTriplet(int[] arr) {
+        int l = arr.length;
+        if (l < 3)
+            return -1;
+        Arrays.sort(arr);
+        return Math.max(arr[l - 1] * arr[l - 2] * arr[l - 3], arr[0] * arr[1] * arr[l - 1]);
+    }
+}
