b76587b20db9cd1574921306abf8c7ab2a81db51
Karan Chaudhary <karan.c.aux@go-jek.com>
2021-06-08 16:55:28 +0530

    Karan | Added a test to sort string case insensitively

---
+++ src/test/java/CharInsensitiveSorting.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CharInsensitiveSorting {
+
+    public static void main(String[] args) {
+        String input = "bcaCzBklABC";
+        System.out.println(sortStringInsensitively(input));
+    }
+
+    static String sortStringInsensitively(String in) {
+        List<String> chars = new ArrayList<>();
+        for(char each: in.toCharArray())
+            chars.add(Character.toString(each));
+        chars.sort(String.CASE_INSENSITIVE_ORDER);
+        StringBuilder builder = new StringBuilder();
+        chars.forEach(builder::append);
+        return builder.toString();
+    }
+}
--- src/test/java/recursion/Palindrome.java
+++ src/test/java/recursion/Palindrome.java
@@ -13,9 +13,8 @@ public class Palindrome {
     }
 
     boolean palindromeCheck(String s, int l, int r) {
-        if (l >= r) {
+        if (l >= r)
             return true;
-        }
         if (s.charAt(l) != s.charAt(r))
             return false;
         return palindromeCheck(s, l + 1, r - 1);
